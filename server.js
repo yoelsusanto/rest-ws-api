92fd803de00b9384064797e396ba4473d4d1e852
Yoel <yoelsusantopro@yahoo.com>
2019-09-27 22:25:34 +0700

    Add WebSocket Implementation

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const http = require("http");
+const web_socket = require("ws");
 
 const express_app = express();
 express_app.use(express.json());
 
 const server = http.createServer(express_app);
 
+const web_socket_server = new web_socket.Server({
+  server: server,
+  path: "/websocket"
+});
+
 let message_list = [];
+let ws_connections = [];
+
+web_socket_server.on("connection", new_ws_connection => {
+  ws_connections.push(new_ws_connection);
+
+  new_ws_connection.on("message", () => {
+    if (message_list.length > 0) {
+      new_ws_connection.send(JSON.stringify(message_list));
+    }
+  });
+});
 
 express_app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
@@ -20,10 +37,16 @@ express_app.post("/message", (req, res) => {
   message = req.body.message;
   message_list.push(message);
 
+  // alternatively can use broadcast
+  ws_connections.forEach(connection => {
+    connection.send(JSON.stringify(message_list));
+  });
+
   res.json({ success: true });
 });
 
-const SERVER_PORT = 3000 || process.env.PORT;
+const SERVER_PORT = 3000;
+
 server.listen(SERVER_PORT, () => {
   console.log(`Server is listening on port: ${SERVER_PORT}`);
 });
